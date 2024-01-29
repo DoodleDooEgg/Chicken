276746c24d658686a44a7b08ce67440e6ac6f607
DoodleDooEgg <126722591+DoodleDooEgg@users.noreply.github.com>
2024-01-29 13:04:46 +0000

    Update Chat.js

--- Chat.js
+++ Chat.js
@@ -1,1 +1,12 @@
-
+var firebaseConfig = {
+  apiKey: "xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx",
+  authDomain: "xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx",
+  databaseURL: "xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx",
+  projectId: "xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx",
+  storageBucket: "xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx",
+  messagingSenderId: "xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx",
+  appId: "xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx",
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+const username = prompt
